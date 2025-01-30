951cc589ebdf00c2d546c91e057c9d40f6132e8c
Ajay Reddy <ajayreddy.karedla@gmail.com>
2025-01-30 14:22:51 +0530

    created ping api

--- server.js
+++ server.js
@@ -11,6 +11,19 @@ app.use(express.json());
 app.use("/users", userRouter);
 app.use("/chats", chatRouter);
 
+app.use("/ping", (req, res) => {
+  try {
+    console.log("ping");
+    res.send("Pinging");
+  } catch (err) {
+    console.log(err);
+    res.send(err || "error");
+  }
+});
+
 app.listen(process.env.PORT || 3000, () => {
   console.log("server started at http://localhost:3000");
+  setInterval(async () => {
+    await fetch("https://chatapp-backend-isfi.onrender.com/ping");
+  }, 300000);
 });
