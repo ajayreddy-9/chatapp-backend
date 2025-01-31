f89d7590b5f85334da7b100271063516aa3c6da5
Ajay Reddy <ajayreddy.karedla@gmail.com>
2025-01-31 16:28:19 +0530

    updated jwt authentication

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -1,52 +1,11 @@
+//authRouter.js
+
 import express, { request } from "express";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 import "dotenv/config";
 import { User } from "../db/models.js";
 const authRouter = express.Router();
-
-export const authenticateToken = (req, res, next) => {
-  try {
-    const authHeader = req.headers.authorization;
-    // console.log(req.headers);
-    if (!authHeader || !authHeader.startsWith("Bearer ")) {
-      return res
-        .status(401)
-        .json({ error: "Access denied. No token provided." });
-    }
-    const jwtToken = authHeader.split(" ")[1];
-    jwt.verify(jwtToken, process.env.JWTSECRETKEY, async (error, payload) => {
-      if (error) {
-        return res.status(401).send("Invalid access token.");
-      } else {
-        console.log(payload);
-        req.phone = payload.phone;
-        next();
-      }
-    });
-    // const decoded = jwt.verify(jwtToken, process.env.JWTSECRETKEY);
-    // console.log(decoded);
-    // next();
-  } catch (err) {
-    console.log(err);
-    res.status(400).send(err.message || "Error verifing token.");
-  }
-};
-
-authRouter.get("/getuser", authenticateToken, async (req, res) => {
-  try {
-    const { phone } = req;
-    const user = await User.findOne({ phone: phone }, { password: 0 });
-    if (!user) {
-      return res.status(401).send("Invalid access token.");
-    } else {
-      return res.status(200).send(user);
-    }
-  } catch (err) {
-    console.log(err);
-    res.status(400).send("Error fetching user details.");
-  }
-});
 
 authRouter.post("/signup", async (req, res) => {
   try {
@@ -94,6 +53,7 @@ authRouter.post("/signin", async (req, res) => {
         phone: phone,
       };
       const jwtToken = jwt.sign(payload, process.env.JWTSECRETKEY);
+      res.cookie("userToken2", jwtToken, { path: "/auth/getuser" });
       res.send({ jwtToken });
     } else {
       res.send("Password Not Matched");
@@ -101,6 +61,49 @@ authRouter.post("/signin", async (req, res) => {
   } catch (err) {
     console.log(err);
     res.status(400).send(err.message || "Error signing in.");
+  }
+});
+
+export const authenticateToken = (req, res, next) => {
+  try {
+    const authHeader = req.headers.authorization;
+    // console.log(req.headers);
+    if (!authHeader || !authHeader.startsWith("Bearer ")) {
+      return res
+        .status(401)
+        .json({ error: "Access denied. No token provided." });
+    }
+    const jwtToken = authHeader.split(" ")[1];
+    jwt.verify(jwtToken, process.env.JWTSECRETKEY, async (error, payload) => {
+      if (error) {
+        return res.status(401).send("Invalid access token.");
+      } else {
+        console.log(payload);
+        req.phone = payload.phone;
+        next();
+      }
+    });
+    // const decoded = jwt.verify(jwtToken, process.env.JWTSECRETKEY);
+    // console.log(decoded);
+    // next();
+  } catch (err) {
+    console.log(err);
+    res.status(400).send(err.message || "Error verifing token.");
+  }
+};
+
+authRouter.get("/getuser", authenticateToken, async (req, res) => {
+  try {
+    const { phone } = req;
+    const user = await User.findOne({ phone: phone }, { password: 0 });
+    if (!user) {
+      return res.status(401).send("Invalid access token.");
+    } else {
+      return res.status(200).send(user);
+    }
+  } catch (err) {
+    console.log(err);
+    res.status(400).send("Error fetching user details.");
   }
 });
 
--- server.js
+++ server.js
@@ -6,7 +6,7 @@ import chatRouter from "./routes/chatRoute.js";
 import authRouter from "./routes/authRouter.js";
 
 const app = express();
-app.use(cors());
+app.use(cors({ credentials: true, methods: ["GET", "POST", "PUT", "DELETE"] }));
 app.use(express.json());
 
 app.use("/users", userRouter);
